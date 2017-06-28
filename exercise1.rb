b6fb077284a535c9340d5612b3124634cd222ef1
heybrettbell <brett@grassrootsonline.ca>
2017-06-28 15:51:32 -0400

    Exercise 0 & 1 complete

---
+++ exercise1.rb
@@ -0,0 +1,27 @@
+#Arrays
+fav_colours = ["blue", "orange", "brown", "grey"]
+ages = [43, 45, 11]
+coin_flip = ["heads", "heads", "tails", "heads", "tails"]
+fav_artists = ["Kendrick Lamar", "Jay-Z", "Drake"]
+fav_colours_symbols = [:blue, :orange, :brown, :grey]
+
+#Hashes
+definitions = {"giddy" => "To be overly happy", "car" => "A motorized passenger vehicle", "house" => "A residential building"}
+fav_movies = {"Shawshank Redemption" => "1990", "The Matrix" => "1999", "Contact" => "1994"}
+cities_population = {"Toronto" => "6.4M", "Chicago" => "2.7M", "Vancouver" => "631 Thousand"}
+names_and_ages = {"Brett" => "43", "Jen" => "45", "Sophie" => "11"}
+
+# Part 1
+puts coin_flip
+
+#Part 2
+puts fav_colours[0]
+
+#Part 3
+puts ages.sort
+
+#Part 4
+ages << 0
+
+#Part 5
+puts fav_movies["The Matrix"]
