c606b134d1eeb1e4726094f38c0e7e578093a42d
heybrettbell <brett@grassrootsonline.ca>
2017-06-28 18:43:13 -0400

    Exercise 5 complete

--- exercise1.rb
+++ exercise1.rb
@@ -8,75 +8,104 @@ fav_colours_symbols = [:blue, :orange, :brown, :grey]
 #Hashes
 definitions = {"giddy" => "To be overly happy", "car" => "A motorized passenger vehicle", "house" => "A residential building"}
 fav_movies = {"Shawshank Redemption" => "1990", "The Matrix" => "1999", "Contact" => "1994"}
-cities_population = {"Toronto" => "6.4M", "Chicago" => "2.7M", "Vancouver" => "631 Thousand"}
-names_and_ages = {"Brett" => "43", "Jen" => "45", "Sophie" => "11"}
+names_and_ages = {:Brett => 43, :Jen => 45, :Sophie => 11}
+cities_population = {"Toronto" => 3100000, "Chicago" => 2700000, "Vancouver" => 631000}
 
-# Part 1
-puts coin_flip
 
-#Part 2
-puts fav_colours[0]
+# # Part 1
+# puts coin_flip
+#
+# #Part 2
+# puts fav_colours[0]
+#
+# #Part 3
+# puts ages.sort
+#
+# #Part 4
+# ages << 0
+#
+# #Part 5
+# fav_movies["The Matrix"]
+#
+# #Exercise 2
+#
+# #Part 1
+# puts fav_colours[-1]
+#
+# #Part 2
+# cities_population["New York"] = "8.4M"
+#
+# #Part 3
+# coin_flip.reverse
+#
+# #Part 4
+# cities_population["Toronto"]
+#
+# #Part 5
+# fav_artists.each do |artist|
+# puts "I think #{artist} is great."
+# end
+#
+# #Exercise 3
+#
+# #Part 1
+# puts fav_artists[0..1]
+#
+# #Part 2
+# fav_movies.each do |movie, year|
+#   puts "#{movie} came out in #{year}."
+# end
+#
+# #Part 3
+# puts ages.sort.reverse
+#
+# #Part 3.1
+# puts ages.sort.reverse.join(' ')
+#
+# #Part 4
+# fav_movies["Beauty and the Beast"] = "1991", "2017"
+#
+# #Exercise 4
+#
+# #Part 1
+# puts ages.select{|x| x < 30}
+#
+# #Part 2
+# puts ages.max
+#
+# #Part 3
+# puts coin_flip.count("heads")
+#
+# #Part 4
+# fav_artists.delete("Drake")
+#
+# #Part 5
+# cities_population["Toronto"] = "9.8M"
+#
+# #Exercise 5
 
-#Part 3
-puts ages.sort
+# #Part 1
+puts cities_population.values.sum
 
-#Part 4
-ages << 0
 
-#Part 5
-fav_movies["The Matrix"]
-
-#Exercise 2
-
-#Part 1
-puts fav_colours[-1]
-
-#Part 2
-cities_population["New York"] = "8.4M"
-
-#Part 3
-coin_flip.reverse
-
-#Part 4
-cities_population["Toronto"]
-
-#Part 5
-fav_artists.each do |artist|
-puts "I think #{artist} is great."
+# Part 2
+names_and_ages.each do |k, v|
+  if v < 40
+    puts "#{k} is old."
+  else
+    puts "#{k} is young."
+end
 end
 
-#Exercise 3
+#Part 3
+puts fav_colours.last(2)
 
-#Part 1
-puts fav_artists[0..1]
-
-#Part 2
-fav_movies.each do |movie, year|
-  puts "#{movie} came out in #{year}."
+#Part 4
+ages.each do |age|
+  puts age + 1
 end
 
-#Part 3
-puts ages.sort.reverse
-
-#Part 3.1
-puts ages.sort.reverse.join(' ')
-
 #Part 4
-fav_movies["Beauty and the Beast"] = "1991", "2017"
-
-#Exercise 4
-
-#Part 1
-puts ages.select{|x| x < 30}
-
-#Part 2
-puts ages.max
-
-#Part 3
-puts coin_flip.count("heads")
-
-#Part 4
-fav_artists.delete("Drake")
+fav_colours += ["red","black"]
 
 #Part 5
-cities_population["Toronto"] = "9.8M"
